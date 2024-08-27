3dc0057ade3599580209dfa048c0c7f44a2fcbe3
Jose Soloaga <josesoloagam@gmail.com>
2024-08-27 18:57:10 +0200

    feat: test recomendation context

--- recomendation-app/src/App.jsx
+++ recomendation-app/src/App.jsx
@@ -1,12 +1,22 @@
-
+import { useRecomendation } from "./modules/recomendation/providers/useRecomendation";
 
 function App() {
-
-
+  const { categories, setCategorie } = useRecomendation();
+  if (categories.length < 1) {
+    return <h1>Loadding...</h1>;
+  }
   return (
     <>
+<input onChange={(event)=>setCategorie(event.target.value)}/>
+
+      {categories.map((category) => (
+        <div key={category.name}>
+          <h1>{category.name}</h1>
+        </div>
+
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- recomendation-app/src/main.jsx
+++ recomendation-app/src/main.jsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { RecomendationProvider } from "./modules/recomendation/providers/RecomendationProvider.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RecomendationProvider>
+      <App />
+    </RecomendationProvider>
   </StrictMode>,
-)
+);
